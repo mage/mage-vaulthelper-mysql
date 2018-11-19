1d0ddce03f2c10fc013652c8b75fac8bc98f7016
stelcheck <mtrudel@wizcorp.jp>
2018-11-19 15:17:18 +0900

    query string escaping

--- src/index.ts
+++ src/index.ts
@@ -197,11 +197,11 @@ export function getVaultClient(vaultName: string): mysql.Pool {
  * @param vaultName
  * @param queryString
  */
-export async function query<T>(vault: any, queryString: string): Promise<T> {
+export async function query<T>(vault: any, queryString: string, data: any[] = []): Promise<T> {
     const client = typeof vault === 'string' ? getVaultClient(vault) : vault as mysql.Pool
 
     return new Promise<T>((resolve, reject) => {
-      client.query(queryString, (error, results) => {
+      client.query(queryString, data, (error, results) => {
         if (error) {
           return reject(error)
         }
